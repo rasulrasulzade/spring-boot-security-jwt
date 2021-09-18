a086dd3aea7bc8f93f647105d277f5bab3fdb834
rasul.rasulzade <rasul.rasulzade@cybernet.az>
2021-09-18 09:57:08 +0400

    First commit

---
+++ src/main/java/com/company/springbootsecurityjwt/model/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.company.springbootsecurityjwt.model;
+
+public class AuthenticationResponse {
+    private String jwt;
+
+    public AuthenticationResponse(String jwt){
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
---
+++ src/main/java/com/company/springbootsecurityjwt/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.company.springbootsecurityjwt.security;
+
+import com.company.springbootsecurityjwt.filter.JwtRequestFilter;
+import com.company.springbootsecurityjwt.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import sun.security.util.Password;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
